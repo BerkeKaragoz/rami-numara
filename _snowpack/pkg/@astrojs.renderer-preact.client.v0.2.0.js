95f02f595ac891e2f8b55ca506c6bdeb850c6098
BerkeKaragoz <e.berkekaragoz@gmail.com>
2021-09-09 10:13:06 +0000

    Deploy https://github.com/BerkeKaragoz/rami-numara/commit/778d37c2d5045d8afc07ad463151eefc8b4e5a34

---
+++ _snowpack/pkg/@astrojs.renderer-preact.client.v0.2.0.js
@@ -0,0 +1,1 @@
+import{v as e,q as t}from"../../preact.v10.5.14-b2c563ed.js";const a=({value:t})=>t?e("astro-fragment",{dangerouslySetInnerHTML:{__html:t}}):null;a.shouldComponentUpdate=()=>!1;var o=o=>(r,l,n)=>t(e(r,l,e(a,{value:n})),o);export{o as default};
